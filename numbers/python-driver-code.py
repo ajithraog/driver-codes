11c6151b6064709b4c5ed35d74d9a935ecce4624
ajithraog <103244116+ajithraog@users.noreply.github.com>
2022-04-08 12:59:35 +0530

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -1,15 +1,19 @@
-# Python Driver Code
+def solve(N:int)  ->str:
+    s=str(N)
+    div=lens(s)//2
+    if int(N)%2==0:
+        left=s[0:div]
+        right=s[div:]
+    else:
+        left=s[0:div]
+        right=s[div+1:] 
+    if int(left)> int(right):
+       return "magic number"
+    else:
+       return "normal number"
+ T=int(input())
+ for i in range(T):
+     test_case=input()
+     answer=solve(test_case)
+    print(answer)
 
-def solve(n: int) -> str:
-  # Your code goes here
-  # n is the given input
-  return "Special"
-
-# The following snippet reads the input in the required format. 
-# Just complete the solve function above. 
-
-T = int(input())
-for i in range(T):
-  test_case = input()
-  answer = solve(test_case)
-  print(answer)
